d97bf2a1964d366827634644174910a3b0af552f
El mahdi Boulhoujjat <60227776+boulhoujjatmehdi@users.noreply.github.com>
2023-02-27 21:11:04 +0100

    base ground

--- ft_main.c
+++ ft_main.c
@@ -6,7 +6,7 @@
 /*   By: eboulhou <eboulhou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 12:02:55 by eboulhou          #+#    #+#             */
-/*   Updated: 2023/02/25 15:56:51 by eboulhou         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:10:08 by eboulhou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,11 +85,12 @@ t_comm *ft_get_commands(char *str, char **env)
 	char **ret = ft_split(str, '|');
 	int i = 0;
 	t_comm *comms;
-	
+
 	comms = NULL;
 	while(ret[i])
 	{
-		char **com = ft_split(ret[0], ' ');
+		// printf("%s\n************\n", ret[i]);
+		char **com = ft_split(ret[i], ' ');
 		t_comm *tmp = new_comm(com, i);
 		tmp->com = get_right_path(tmp->com, env);
 		add_back_comm(&comms, tmp);
@@ -170,44 +171,44 @@ void funct(t_comm *com, char **env, int token , int file_descriptors[2][2] , int
 #define FT_REDIRECT 3
 
 
-void first_child(int *fd1 , int *fd2, int *fd3, int *pid)
+void first_child(int *fd, int *pid)
 {
 	pid[0] = fork();
 	if(pid[0] == 0)
 	{
-		dup2(fd1[1] , 1);
+		dup2(fd[1] , 1);
 		//close all
-		close(fd1[0]);
-		close(fd1[1]);
-		close(fd2[0]);
-		close(fd2[1]);
-		close(fd3[0]);
-		close(fd3[1]);
+		// printf("mehdi boulhoujjat\n");
+		int ii = 0;
+		while(ii < 10)
+			close(fd[ii++]);
 		//-------------
 		char *c0[3];	c0[0] = strdup("/bin/ls");	c0[1] = strdup("-la");	c0[2] = NULL;
 		t_comm *cc0 = new_comm (c0 , 0);
 		char **env;
 		env = NULL;
+		// sleep(1);
 		execve(cc0->com, cc0->flags, env);
 	}
 }
 
-void middle_child(int *fd1 , int *fd2, int *fd3 , int *pid)
+void middle_child(int *fdd, int *fd, int *pid , int i)
 {
 	pid[1] = fork();
 	if(pid[1] == 0)
 	{
-		dup2(fd1[0], 0);
-		dup2(fd2[1], 1);
+		// sleep(8);
+		dup2(fdd[0], 0);
+		dup2(fdd[3], 1);
 		//close all
-		close(fd1[0]);
-		close(fd1[1]);
-		close(fd2[0]);
-		close(fd2[1]);
-		close(fd3[0]);
-		close(fd3[1]);
+		int ii = 0;
+		while(ii < 10)
+			close(fd[ii++]);
 		//-------------
-		char *c1[3];	c1[0] = strdup("/usr/bin/grep");	c1[1] = strdup("eboulhou");	c1[2] = NULL;
+		char str[100];
+		str[0] = '0' + i;
+		str[1] = 0;
+		char *c1[3];	c1[0] = strdup("/usr/bin/grep");	c1[1] = strdup(str);	c1[2] = NULL;
 		t_comm *cc1 = new_comm (c1 , 1);
 		char **env;
 		env = NULL;
@@ -215,56 +216,89 @@ void middle_child(int *fd1 , int *fd2, int *fd3 , int *pid)
 	}
 }
 
-void third_child(int *fd1 , int *fd2, int *fd3 , int *pid)
+void third_child(int *fdd ,int *fd, int *pid)
 {
 	pid[2] = fork();
 	if(!pid[2])
 	{
-		dup2(fd2[0], 0);
+		dup2(fdd[0], 0);
 		//close all
-		close(fd1[0]);
-		close(fd1[1]);
-		close(fd2[0]);
-		close(fd2[1]);
-		close(fd3[0]);
-		close(fd3[1]);
+		int ii = 0;
+		while(ii < 10)
+			close(fd[ii++]);
 		//-------------
-		char *c2[3];	c2[0] = strdup("/usr/bin/grep");	c2[1] = strdup("1");	c2[2] = NULL;
+		
+		char *c2[3];	c2[0] = strdup("/usr/bin/wc");	c2[1] = strdup("-l");	c2[2] = NULL;
 		t_comm *cc2 = new_comm (c2 , 2);
 		char **env;
 		env = NULL;
 		execve(cc2->com, cc2->flags, env);
 	}
+
 }
 
 
-int main()
+
+
+//initialize the t_minishell data by opening the pipes needed
+//set the number of pipes needed
+//pipe_nb == 0, no pipe needed
+//pipe_nb == -1, no command
+void initialize_data(t_minishell *msh)
 {
-	int i = 0;
+	int i;
 	
-	//open pipes
-	int fd1[2];
-	int fd2[2];
-	int fd3[2];
-	
-	pipe(fd1);
-	pipe(fd2);
-	pipe(fd3);
-	//-----------
-	int pid[3] = {0};
-	first_child(fd1, fd2, fd3, pid);
-	middle_child(fd1, fd2, fd3, pid);
-	third_child(fd1, fd2, fd3, pid);
-	
-	close(fd1[0]);
-	close(fd1[1]);
-	close(fd2[0]);
-	close(fd2[1]);
-	close(fd3[0]);
-	close(fd3[1]);
-	waitpid(-1 , NULL, 0);
-	
-	
+	msh->pipe_nb = get_comm_lenght(msh->comms) - 1;
+	msh->pipe = malloc (sizeof(int) * msh->pipe_nb);
+	i = 0;
+	while(i < msh->pipe_nb)
+	{
+		pipe(&msh->pipe[i * 2]);
+		i++;
+	}
+}
+
+int *open_pipes(int nb)
+{
+	int *fd;
+	int i;
+
+	i = 0;
+	fd = malloc(sizeof(int) * nb);
+	if(!fd)
+		return NULL;
+	while(i < nb)
+	{
+		pipe(&fd[ i * 2]);
+		i++;
+	}
+	return fd;
+}
+// void fork_it_for_me()
+
+int main(int ac , char **av, char **env) 
+{
+	char *str;
+	int i;
+
+	while(1)
+	{
+		t_minishell msh;
+
+		str = readline("minishell $>");
+		add_history(str);
+		msh.comms = ft_get_commands(str, env);
+		initialize_data(&msh);
+		for(int j = 0; j < 3; j++)
+		{
+			printf("com = %s **||** flags = %s\n" , msh.comms->com ,msh.comms->flags[1]);
+			msh.comms = msh.comms->next;
+		}
+
+		
+		
+	}
+	return 0;
 }
 
 
@@ -329,68 +363,123 @@ int main()
 // 	return 0;
 // }
 
-// int main(int ac , char **av, char **env) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main()
 // {
-// 	char *str;
-// 	int	fd[2];
-// 	int ids[5];
-// 	int i;
-// 	ids[0] = 0;
-// 	while(1)
+// 	int i = 0;
+	
+// 	//open pipes
+// 	int fd[100];
+// 	pipe(&fd[0]);
+// 	pipe(&fd[2]);
+// 	pipe(&fd[4]);
+// 	pipe(&fd[6]);
+// 	pipe(&fd[8]);
+// 	//-----------
+// 	int pid[3] = {0};
+// 	first_child(fd, pid);
+// 	while(i < 3)
 // 	{
-// 			t_comm *comms;
-
-// 			str = readline("minishell $>");
-// 			comms = ft_get_commands(str, env);
-// 			// pipe(fd);
-
-// 		i = 0;
-// 		while (i < get_comm_lenght(comms))
-// 		{
-// 			if(!ids[0])
-// 			{
-// 				ids[i] = fork();
-// 				printf("---i = %d-ids[%d] == %d--\n", i, i, ids[i]);
-// 			}
-// 			i++;
-// 		}
-// 		i = 0;
-// 		// printf();
-// 		// ids[0] = fork();
-// 		// while(i--)
-// 		// {
-// 			if(!ids[0])
-// 			{
-// 				// close(fd[0]);
-// 				// dup2(fd[1], 1);
-// 				// puts(comms->com);
-// 				// wait(NULL);
-// 				puts("-------------------------\n");
-// 				exit(0);
-// 				// execve(comms->com, comms->flags, env);
-// 				// puts("mehdi\n");
-// 				// close(fd[1]);
-// 			}
-// 		// }
-// 		if(ids[0])
-// 		{
-// 			// close(fd[0]);
-// 			// wait(NULL);
-// 			wait(NULL);
-// 			puts("puts from parent process\n");
-// 			// comms = NULL;
-// 			// close(fd[1]);
-// 		}
-
-
-
-		
+// 		middle_child(&fd[i*2], fd, pid , i*2);
+// 		printf("%d -- is the fd\n", fd[i*2]);
+// 		i++;
 // 	}
-// 		// t_comm *tmp = comms;
-// 		// while(tmp)
-// 		// {
-// 		// 	printf("%s -- %s -- %s\n",tmp->com, tmp->flags[1], tmp->flags[2]);
-// 		// 	tmp = tmp->next;
-// 		// }
-// 	return 0;
+	
+// 	third_child(&fd[(i)*2],fd ,  pid);
+// 	int ii = 0;
+// 	while(ii < 10)
+// 		close(fd[ii++]);
+// 	for (int j=0; j < 5; j++)
+// 	{
+// 		waitpid(-1 , NULL, 0);
+// 	}
+
 // }
--- ft_minishell.h
+++ ft_minishell.h
@@ -6,7 +6,7 @@
 /*   By: eboulhou <eboulhou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 12:04:02 by eboulhou          #+#    #+#             */
-/*   Updated: 2023/02/21 20:24:17 by eboulhou         ###   ########.fr       */
+/*   Updated: 2023/02/27 20:58:59 by eboulhou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,26 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-// typedef struct s_flag
-// {
-// 	int idx;
-// 	char *flg;
-// 	struct s_flag *next;
-// }t_flag;
+#define NX_PIPE
+#define NX_OUT
+#define PR_PIPE
+#define PR_IN
+#define PR_FRST
+
 
 typedef struct s_comm
 {
 	int idx;
 	char *com;
 	char **flags;
+	int nxt;
 	struct s_comm *next;
-	struct s_comm *prev;
 }t_comm;
-
+typedef struct s_minishell
+{
+	t_comm *comms;
+	int *pipe;
+	int pipe_nb;
+}t_minishell;
 
 #endif
