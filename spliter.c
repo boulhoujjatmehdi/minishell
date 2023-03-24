f6f3dd8abeca26efcfc49dcf1e2a934f1e06d7c2
fhihi <fouadhihi9@gmail.com>
2023-03-24 18:00:02 +0000

    here

--- spliter.c
+++ spliter.c
@@ -6,7 +6,7 @@
 /*   By: fhihi <fhihi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 15:25:50 by fhihi             #+#    #+#             */
-/*   Updated: 2023/03/20 10:17:18 by fhihi            ###   ########.fr       */
+/*   Updated: 2023/03/23 18:04:09 by fhihi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	is_not_arg(char c)
 {
-	if (c == ' ' || c == '|' || c == '<' || c == '>')
+	if (c == '|' || c == '<' || c == '>')
 		return (1);
 	return (0);
 }
@@ -82,83 +82,100 @@ int cont(char *s)
 	return (count);
 }
 
-char	**spliter(char *s)
+int	ft_has_red(char *s)
 {
-	int	i, j, i0;
-	char **new;
-	int count;
+	if (ft_strchr(s, '<') || ft_strchr(s, '>'))
+		return (1);
+	return (0);
+}
 
-	count = 0;
-	i = 0;
-	j = 0;
-	s = ft_strtrim(s, " 	");
-	i0 = i;
-	new = (char **)malloc((cont(s) + 1) * sizeof(char *));
+char	**get_red(char *s)
+{
+	int i;
+	int len;
+	char **new;
+
+	new = (char **)malloc(2 * sizeof(char *));
 	if (!new)
 		return (NULL);
+	if (is_not_arg(s[0]))
+	{
+		new[1] = ft_strdup(s + 1);
+		printf("this ;%s;\n", new[1]);
+		puts("here");
+		new[0][0] = s[0];
+		new[0][1] = '\0';
+	}
+	else
+	{
+		len = ft_strlen(s) - 2;
+		new[0] = ft_strdup(s);
+		new[0][len] = '\0';
+		new[1][0] = s[len];
+		new[1][1] = '\0';
+	}
+	return (new);
+}
+
+char	**spliter(char *s)
+{
+	int	i, j;
+	char **new;
+	char **end;
+
+	new = ft_split(s, ' ');
+	i = 0;
+	j = 0;
+	while (new[i])
+	{
+		if (ft_has_red(new[i]) && ft_strlen(new[i]) > 2)
+		{
+			
+		}
+		else
+			end[j] = new[i];
+		i++;
+	}
+	return (new);
+}
+
+char	**spliter2(char *s)
+{
+	char **new;
+	char tmp[256];
+	int i, j, k, count, t, t1;
+	
+	count = cont(s);
+	new = (char **)malloc((count + 1) * sizeof(char *));
+	if (!new)
+		return (NULL);
+	i = 0;
+	j = 0;
+	k = 0;
 	while (i <= ft_strlen(s))
 	{
 		if (is_not_arg(s[i]))
 		{
-			if (is_not_arg(s[i - 1]))
-			{
-				if (check_red(s[i], s[i + 1]) == 1)
-				{
-					new[j] = ft_substr(s, i0, i - i0);
-					j++;
-					new[j] = ft_substr(s, i, 1);
-					j++;
-					i++;
-					i0 = i;
-				}
-				else 
-				{
-					new[j] = ft_substr(s, i0, i - i0);
-					j++;
-					new[j] = ft_substr(s, i, 2);
-					j++;
-					i+= 2;
-					i0 = i;
-				}
-			}
-			else if (!(is_not_arg(s[i - 1])))
-			{
-				if (check_red(s[i], s[i + 1]) == 1)
-				{
-					new[j] = ft_substr(s, i0, 1);
-					j++;
-					i++;
-					i0 = i;
-				}
-				else if (check_red(s[i], s[i + 1]) == 2)
-				{
-					new[j] = ft_substr(s, i0, 2);
-					j++;
-					i+= 2;
-					i0 = i;
-				}
-			}
-
-		}
-		else if (s[i] == '\0')
-		{
-			new[j] = ft_substr(s, i0, i - i0);
+			tmp[k] = '\0';
+			new[j] = ft_strdup(tmp);
+			k = 0;
+			t = 0;
 			j++;
-			i++;
 		}
-		else
-			i++;
+		tmp[k] = s[i];
+		i++;
+		k++;
 	}
-	new[j] = NULL;
 	return (new);
 }
 
 int main (int ac, char **av)
 {
 	char **new;
-	printf("len = %d\n", cont(av[1]));
-	// exit(0);
+	int i = 0;
+
 	new = spliter(av[1]);
-	for(int i = 0; i < cont(av[1]); i++)
-		printf(":%s:\n", new[i]);
+	// exit(0);
+	while (new[i])
+		printf(":%s:\n", new[i++]);
 }
